5451831109f5e7f48cecd2db44c064b7ba74313a
Joe Saraceno <honestcomrade@gmail.com>
2017-07-28 21:28:46 -0700

    add styles

--- src/components/pages/booksList.js
+++ src/components/pages/booksList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {getBooks} from '../../actions/booksActions'
+import {Grid, Row, Button} from 'react-bootstrap';
 
 class BooksList extends React.Component {
   componentDidMount(){
@@ -15,13 +16,20 @@ class BooksList extends React.Component {
           <h2>{booksArr.title}</h2>
           <h2>{booksArr.description}</h2>
           <h2>{booksArr.price}</h2>
+          <Button bsStyle='primary' style={{marginLeft:'10px'}}>Add to Cart</Button>
         </div>
       )
     })
     return(
       <div>
-        {BooksList}
-      </div>
+        <h1 style={{margin:'10px', fontStyle: 'italic', fontWeight:'700', textDecoration:'underline'}}>Get Your Books!</h1>
+      
+        <Grid>
+          <Row style={{marginTop:'15px', marginLeft:'20px'}}>
+            {BooksList}
+          </Row>
+        </Grid>  
+      </div> 
     )
   }
 }
