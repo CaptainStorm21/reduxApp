10a75dd598459df34fb1628f8ba7cca8381c166f
Joe Saraceno <honestcomrade@gmail.com>
2017-07-30 21:24:52 -0700

    router

--- src/app.js
+++ src/app.js
@@ -1,27 +1,39 @@
 "use strict"
 
+// libraries
 import React from 'react';
 import logger from 'redux-logger';
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 import {applyMiddleware, createStore} from 'redux';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
 
+// actions
 import {postNewBooks, deleteBooks, updateBooks} from './actions/booksActions';
 import {addToCart} from './actions/cartActions';
-import BooksList from './components/pages/booksList.js';
 import reducers from './reducers/index';
-import Menu from './components/menu';
-import Footer from './components/footer';
+
+// components
+import BooksList from './components/pages/booksList';
+import Cart from './components/pages/cart';
+import BooksForm from './components/pages/booksForm';
+import Main from './main';
 
 const middleware = applyMiddleware(logger);
 const store = createStore(reducers, middleware);
 
-render(
+const Routes = (
   <Provider store={store}>
-    <div>
-      <Menu />
-      <BooksList />
-      <Footer /> 
-    </div>
-  </Provider>, document.getElementById('app')
+    <Router history={browserHistory}>
+      <Route path="/" component={Main}>
+        <IndexRoute component={BooksList} />
+          <Route path="/admin" component={BooksForm} />
+          <Route path="/cart" component={Cart} />
+      </Route>
+    </Router>
+  </Provider>
+)
+
+render(
+  Routes, document.getElementById('app')
 );
